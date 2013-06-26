56de8c9b9dd713aedca17bdf28edb0a0a599d6c3
TheBr0ken <jtoro2716@gmail.com>
2013-06-26 10:43:14 -0500

    libhardware: Enable broadcast stream for audio hardware with STE_SAMSUNG_HARDWARE

--- include/hardware/audio.h
+++ include/hardware/audio.h
@@ -570,8 +570,8 @@ struct audio_hw_device {
     void (*close_output_stream)(struct audio_hw_device *dev,
                                 struct audio_stream_out* stream_out);
 
-#ifdef QCOM_HARDWARE
-    /** This method creates and opens the audio hardware output
+#if defined (QCOM_HARDWARE) || defined (STE_SAMSUNG_HARDWARE)
+/** This method creates and opens the audio hardware output
      *  for broadcast stream */
     int (*open_broadcast_stream)(struct audio_hw_device *dev, uint32_t devices,
                                  int format, uint32_t channels,
